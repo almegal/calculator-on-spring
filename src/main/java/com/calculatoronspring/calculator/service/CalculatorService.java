673e739ac53b7a61e55f53334a8942ce69c4a72c
alekseipeikhel <alekseipeikhel@gmail.com>
2024-03-15 01:05:37 +0300

    Revert "Revert "initial commit""
    This reverts commit 607f363fe667a2b4c77e274c813057d1b1b95206.

---
+++ src/main/java/com/calculatoronspring/calculator/service/CalculatorService.java
@@ -0,0 +1,8 @@
+package com.calculatoronspring.calculator.service;
+
+public interface CalculatorService {
+    public String add(Integer a, Integer b);
+    public String minus(Integer a, Integer b);
+    public String divide(Integer a, Integer b);
+    public String multiply(Integer a, Integer b);
+}
---
+++ src/test/java/com/calculatoronspring/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculatoronspring.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
