c636be74b03e7515bfd90d0acade3b336c56ff46
alekseipeikhel <alekseipeikhel@gmail.com>
2024-03-14 11:36:47 +0300

    created interface for CalculatorService

--- src/main/java/com/calculatoronspring/calculator/service/CalculatorService.java
+++ src/main/java/com/calculatoronspring/calculator/service/CalculatorService.java
@@ -1,4 +1,8 @@
 package com.calculatoronspring.calculator.service;
 
 public interface CalculatorService {
+    public int add(int a, int b);
+    public int minus(int a, int b);
+    public int divide(int a, int b);
+    public int multiply(int a, int b);
 }
