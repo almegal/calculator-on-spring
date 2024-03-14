495487993e877dbf9568b06e3954353a95fbc81e
alekseipeikhel <alekseipeikhel@gmail.com>
2024-03-15 01:04:41 +0300

    Revert "initial commit"
    This reverts commit a0f1d6c0

--- src/main/java/com/calculatoronspring/calculator/service/CalculatorService.java
+++
@@ -1,8 +0,0 @@
-package com.calculatoronspring.calculator.service;
-
-public interface CalculatorService {
-    public String add(Integer a, Integer b);
-    public String minus(Integer a, Integer b);
-    public String divide(Integer a, Integer b);
-    public String multiply(Integer a, Integer b);
-}
